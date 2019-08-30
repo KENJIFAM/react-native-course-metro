6699db02ffcab25ba0221de3f27814afe27ae7ba
khangnguyen <khang.nguyen@giosg.com>
2019-08-30 16:11:49 +0300

    Revert

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -57,6 +57,40 @@ const FavNavigator = createStackNavigator(
   }
 );
 
+const tabScreenConfig = {
+  Places: {
+    screen: PlacesNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return (
+          <MaterialIcons name="location-city" size={25} color={tabInfo.tintColor} />
+        );
+      },
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel:
+        Platform.OS === 'android' ? (
+          <Text style={{ fontFamily: 'open-sans-bold' }}>Places</Text>
+        ) : (
+          'Places'
+        )
+    }
+  },
+  Favorites: {
+    screen: FavNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+      },
+      tabBarColor: Colors.accentColor,
+      tabBarLabel:
+        Platform.OS === 'android' ? (
+          <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text>
+        ) : (
+          'Favorites'
+        )
+    }
+  }
+};
 
 const PlacesFavTabNavigator =
   Platform.OS === 'android'
@@ -81,6 +115,9 @@ const FiltersNavigator = createStackNavigator(
     Filters: FiltersScreen
   },
   {
+    // navigationOptions: {
+    //   drawerLabel: 'Filters!!!!'
+    // },
     defaultNavigationOptions: defaultStackNavOptions
   }
 );
